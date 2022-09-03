078344b8a987822f8db65a73b1176f167f34817d
veeresharihar <112688973+veeresharihar@users.noreply.github.com>
2022-09-03 09:26:33 +0530

    Add files via upload

---
+++ blogpost/__init__.py
@@ -0,0 +1,26 @@
+import os
+import bcrypt
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+from flask_mail import Mail
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY']='a8e2a930711676d088efd64a269d7696'
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///site.db'
+db=SQLAlchemy(app)
+bcrypt= Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
+app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
+mail = Mail(app)
+
+
+from blogpost import routes
